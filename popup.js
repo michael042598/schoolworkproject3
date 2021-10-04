1ce50f7336ac3466ecd7896862cc732e47d8818d
Jan Michael Tan <tan1801260@ceu.edu.ph>
2021-10-04 11:14:57 +0800

    Fix x and y coordinate

--- popup.js
+++ popup.js
@@ -21,8 +21,8 @@ function spawnimage(x, y) {
   image.setAttribute('class', 'overlays');
   image.setAttribute('width', 64);
   image.setAttribute('height', 64);
-  image.style.left = x - 64 + 'px';
-  image.style.top = y - 64 + 'px';
+  image.style.left = x - 32 + 'px';
+  image.style.top = y - 32 + 'px';
   images.push(image);
   document.body.appendChild(image);
 }
