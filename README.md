2d691b6fa728d5e05ffbd9a9384c0607d6a2f2df
Jan Michael Tan <tan1801260@ceu.edu.ph>
2021-10-04 09:59:24 +0800

    Image change

---
+++ README.md
@@ -0,0 +1,10 @@
+# 🐁 trail
+
+> A little project to create an image-based mouse trail using vanilla JavaScript.
+
+<a href='https://trail.now.sh/'><img src="./media/trail.gif" width="846"></a>
+
+## Usage
+1. Fork this repository
+2. Replace `circle.png` with your own image
+3. Host it somewhere! ☁
---
+++ popup.js
@@ -0,0 +1,56 @@
+
+// Store images so we can remove them after they hit an upper limit
+let images = [];
+
+// Store mouse position
+let mousePos = { x: -1000, y: -1000};
+
+let lastX = -1000;
+let lastY = -1000;
+
+function spawnimage(x, y) {
+  if (lastX === x && lastY === y) {
+    return
+  }
+
+  lastX = x;
+  lastY = y;
+
+  var image = document.createElement('img');
+  image.setAttribute('src', './unknown2.png');
+  image.setAttribute('class', 'overlays');
+  image.setAttribute('width', 128);
+  image.setAttribute('height', 128);
+  image.style.left = x - 64 + 'px';
+  image.style.top = y - 64 + 'px';
+  images.push(image);
+  document.body.appendChild(image);
+}
+
+
+function repeater() {
+  setTimeout(function () {
+    spawnimage(mousePos.x, mousePos.y);
+    repeater();
+  }, 10);
+}
+
+document.addEventListener('mousemove', function(e) { 
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+});
+
+function removeImage() {
+  setTimeout(function() {
+    if (images.length > 1) {
+      images.shift().remove();
+    }
+    removeImage();
+  }, 20);
+}
+
+document.addEventListener('DOMContentLoaded', function(event) { 
+  spawnimage(mousePos.x, mousePos.y);
+  repeater();
+  removeImage();
+});
